4330b0057b526037adcde38075a56f526c4b94e9
odeadglaz <oded.goldglas+git@fiver.com>
2020-04-29 22:10:10 +0300

    docs.

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ const Context = {
      * Creates an execution context for the current asyncId process.
      * This will expose Context get / update at any point after.
      * @param {Object} initialContext - The initial context to be used
+     * @returns void
      */
     create: (initialContext = {}) => {
         const asyncId = asyncHooks.executionAsyncId();
@@ -51,6 +52,7 @@ const Context = {
     /**
      * Updates the current async process context
      * @param {Object} update - The update to apply on the current process context
+     * @returns void
      */
     update: (update = {}) => {
         const asyncId = asyncHooks.executionAsyncId();
