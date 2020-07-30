22113bb9aeca395c13147bbe4202b1898112e733
odeadglaz <oded.goldglas+git@fiverr.com>
2020-07-30 18:49:19 +0300

    docs.

--- src/lib/ExecutionContextResource.js
+++ src/lib/ExecutionContextResource.js
@@ -2,6 +2,9 @@ const { AsyncResource } = require('async_hooks');
 
 const ASYNC_RESOURCE_TYPE = 'REQUEST_CONTEXT';
 
+/**
+ * Wraps node AsyncResource with predefined type.
+ */
 class ExecutionContextResource extends AsyncResource {
     constructor() {
         super(ASYNC_RESOURCE_TYPE);
